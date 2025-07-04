95f6d43afadb956ce0aee4b8f1c267cb3fba1ef6
pasidu10 <punnyanamda.thero@gmail.com>
2025-07-04 22:25:21 +0530

    Create Mm.js

---
+++ Mm.js
@@ -0,0 +1,21 @@
+```json
+{
+  "name": "PASIYA-MD",
+  "description": "Javascript WhatsApp bot made by PASIYA",
+  "logo": "",
+  "keywords": ["bot"],
+  "success_url": "/",
+  "env": {
+    "SESSION_ID": {
+      "description": "PASIYA-MD=UEwnnaQA#e9wSoN9gvP9iKtfpX3lNM5sga-Wd86NYTfTfZSJXBhA",
+      "required": true
+    }
+  },
+  "buildpacks": [
+    {
+      "url": "https://github.com/heroku/heroku-buildpack-nodejs.git"
+    }
+  ],
+  "stack": "heroku-24"
+}
+```
